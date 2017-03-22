2044d15ff977c6d5044be539539cb31051740706
Clark Rinker <cgrinker@gmail.com>
2017-03-22 15:59:22 -0700

    Extracting Default State

--- src/index.js
+++ src/index.js
@@ -2,18 +2,12 @@
 
 // Import This First
 import Yarl from 'yarl';
-console.log(yarl);
 
 // Then Load Modules.
 import modules from './modules';
 
 // Gather your default state
-let defaultState = Object.keys(modules).reduce((st, e)=> {
-    if (modules[e].defaultState) {
-      st[e] = modules[e].defaultState
-    }
-    return st;
-}, {})
+const defaultState = Yarl.extractDefaultState(modules);
 
 // Then Invoke Yarl's Entrypoint
 window.Context = Yarl.entrypoint(defaultState);
